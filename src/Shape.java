599ab77222fef0c64f045e57a4f1d200cf2dbf41
Nikmet <metlov.n@mail.ru>
2023-03-01 20:50:46 +0300

    restart

--- src/Shape.java
+++ src/Shape.java
@@ -1,23 +1,5 @@
 import java.awt.*;
 
 public abstract class Shape {
-    private Color color;
 
-    public Shape() {
-        this(new Color(0,0,0));
-    }
-
-    public Shape(Color color) {
-        this.color = color;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    public abstract void draw();
 }
